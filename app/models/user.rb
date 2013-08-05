c6c6521c8a67434558184b647c16cc861ec1633e
Jen Lessor <jen_lessor@yahoo.com>
2013-08-05 13:53:35 -0400

    Add Faker Gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :descriptions
   # attr_accessible :title, :body
 
   has_many :pins
